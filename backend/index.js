c84d505681c9438e73b755a187cd325083213566
Kang HyunWoo <khw7147@kaist.ac.kr>
2018-07-05 15:36:07 +0900

    MongoDB install

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,22 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
+const db = mongoose.connection
+db.on('error', console.error)
+db.once('open', () => {
+	console.log('Connected to MongoDB Server')
+});
+
 // GET request for users
 app.get('/users', (req, res) => {
-	res.json('This is where users will be GET')
+	//res.json('This is where users will be GET')
 });
 
 // PUT request to create/register a new user
 app.put('/users/register', (req, res) => {
-	res.json({'success': true})
+	//res.json({'success': true})
 });
 
 app.listen(port, (err) => {
