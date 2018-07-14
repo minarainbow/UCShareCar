86670ccdeec01d04f8cfcdcf97139f1840873c69
Spencer Peterson <spencerjpeterson@gmail.com>
2018-07-13 23:14:30 -0700

    fix login tests

--- backend/test/test_index.js
+++ backend/test/test_index.js
@@ -4,7 +4,7 @@ const google_login = require('../google_login')
 
 describe('POST /users/login', function(){
 	it('respond with json', function(done) {
-		google_login.verify = () => undefined
+		google_login.verify = async () => { return {name: 'Joe', email: 'joe@x.com'}}
 		request(app)
 			.post('/users/register')
 			.send({token: 'bad_token'})
@@ -16,35 +16,33 @@ describe('POST /users/login', function(){
 			});
 	})
 	it('rejects invalid tokens', function(done) {
-		google_login.verify = () => undefined
+		google_login.verify = async () => { throw new Error("invalid") }
 		request(app)
 			.post('/users/login')
 			.send({token: 'bad_token'})
 			.set('Accept', 'application/json')
 			.expect(200, {
 				success: false,
-				not_registered: true,
+				needs_register: false,
 			})
 			.end(function(err, res){
 				if (err) return done(err);
-				done()
+				else done()
 			});
 	})
-	it('accepts invalid tokens', function(done) {
-		done()
-		// TODO I can't make this test past without a proper database
-		// abstraction
-		/*google_login.verify = () => "fake_user_id"
+	it('asks unregistered users to register', function(done) {
+		google_login.verify = async () => { return {name: 'Joe', email: 'joe@x.com'}}
 		request(app)
 			.post('/users/login')
 			.send({token: 'bad_token'})
 			.set('Accept', 'application/json')
 			.expect(200, {
-				success: true,
+				success: false,
+				needs_register: true,
 			})
 			.end(function(err, res){
 				if (err) return done(err);
 				done()
-			});*/
+			});
 	})
 });
