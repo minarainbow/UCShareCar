adb914162394f9a3986a4e8d6c3334c03ad5d83f
Spencer Peterson <spencerjpeterson@gmail.com>
2018-07-13 18:40:00 -0700

    clean up login handlers and comment a bit

--- backend/index.js
+++ backend/index.js
@@ -27,52 +27,79 @@ app.get('/users', (req, res) => {
 	res.json('This is where users will be GET')
 })
 
-// POST request to create/register a new user
+/*
+ * POST request to create/register a new user
+ * Expects a field "token" in the request body. Verifies with google.
+ * Returns a JSON object with two fields:
+ *	"success": true if the user is now logged in, false otherwise
+ *	"needs_register": true if the user should be prompted to register, false o/w
+ */
 app.post('/users/login', async (req, res) => {
+	// Check if the request actually has a token
+	if (req.body.token === undefined) {
+		console.log("Got a login request w/o a token")
+		res.json({success: false, needs_register: false})
+	}
+
+	// Verify with google
 	const user = await google_login.verify(req.body.token)
 	if (!user) {
 		console.log("Could not verify user, returning fail")
-		res.json({'success': false, 'needs_register': false})
+		res.json({success: false, needs_register: false})
 		return
 	}
-	db.user.cond_registered(user.email, (id) => {
-		// Execued if the user is verified
 
-		console.log("Verified login from", id)
-		sessions.create(res, id)
-		res.json({'success': true, 'needs_register': true})
-	},
-	() => {
-		// Executed if the user is not in the database
-		console.log("Recieved a login from unregistered user. Setting cookies and saving their info.")
+	// If verified, check if they are registered or not
+	else {
+		db.user.cond_registered(user.email, (id) => {
+			// Execued if the user is verified
 
-		// Save their information (w/o phnum)
-		const id = db.user.new({
-			'name': user.name,
-			'email': user.email,
+			console.log("Verified login from", id)
+			sessions.create(res, id)
+			res.json({success: true, needs_register: true})
+		},
+		() => {
+			// Executed if the user is not in the database
+			console.log("Recieved a login from unregistered user. Setting cookies and saving their info.")
+
+			// Save their information (w/o phnum)
+			const id = db.user.new({
+				'name': user.name,
+				'email': user.email,
+			})
+
+			// Set their cookie so we can remember them
+			sessions.create(res, id)
+
+			res.json({success: false, needs_register: true});
 		})
-
-		// Set their cookie so we can remember them
-		sessions.create(res, id)
-
-		res.json({'success': false, 'needs_register': true});
-	})
+	}
 })
 
+/*
+ * Registers a user. Users should have already attempted a login -- this handler
+ * only completes the process of setting up a user. It essentially only adds the
+ * phone number.
+ * It expects a field "phnum" in the request body. This is a string representing
+ * their phone number.
+ * Return value has two fields:
+ *	"success": true if user successfully registered, else false
+ *	"error": an error if there was one, o/w undefined
+ */
 app.post('/users/register', async (req, res) => {
 	if (!sessions.validate(req, res)) return
 
 	// Check for the phone number actually being passed
 	if (req.body.phnum === undefined) {
 		console.log("Missing phone number to register")
-		res.json({'success': false, 'error': 'Did not recieve a phone number'})
+		res.json({success: false, error: 'Did not recieve a phone number'})
 	}
 
 	// Save the phnum
 	console.log("Successfully saving phone number to register", req.signedCookies.session.id)
 	db.user.add_phnum(req.signedCookies.session.id, req.body.phnum)
 	// TODO this success should probably be sent from a callback in the database
-	res.json({'success': true})
+	res.json({success: true})
 })
 
 app.get('/post_list', (req, res) => {
