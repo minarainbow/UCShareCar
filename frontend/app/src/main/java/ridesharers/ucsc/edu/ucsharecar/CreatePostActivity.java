8a766bb149b19834b7693eda5ea18a5898bb15f1
Priya Calaimany <pcalaima@ucsc.edu>
2018-07-16 08:45:41 -0700

    Replaced first spinner

--- frontend/app/src/main/java/ridesharers/ucsc/edu/ucsharecar/CreatePostActivity.java
+++ frontend/app/src/main/java/ridesharers/ucsc/edu/ucsharecar/CreatePostActivity.java
@@ -2,12 +2,36 @@ package ridesharers.ucsc.edu.ucsharecar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class CreatePostActivity extends AppCompatActivity {
+public class CreatePostActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+
+        Spinner originSpinner = findViewById(R.id.origin_spinner);
+        ArrayAdapter<CharSequence> originAdapter = ArrayAdapter.createFromResource(this, R.array.StartingLocations, android.R.layout.simple_spinner_item);
+        originAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        originSpinner.setAdapter(originAdapter);
+        originSpinner.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String origin = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), origin, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
