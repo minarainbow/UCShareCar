cdb753ce9beb6b9bcc5413f70e7e6d53d897ec9d
Kang HyunWoo <khw7147@kaist.ac.kr>
2018-07-20 14:34:21 +0900

    Create Post basic but not tested

--- frontend/app/src/main/java/ridesharers/ucsc/edu/ucsharecar/CreatePostActivity.java
+++ frontend/app/src/main/java/ridesharers/ucsc/edu/ucsharecar/CreatePostActivity.java
@@ -1,23 +1,95 @@
 package ridesharers.ucsc.edu.ucsharecar;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 public class CreatePostActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Context mContext;
+    BackendClient backendClient;
+    Spinner originSpinner, destinationSpinner, seatsSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+        mContext = this;
 
-        Spinner originSpinner = findViewById(R.id.start_spinner);
-        Spinner destinationSpinner = findViewById(R.id.destination_spinner);
+        originSpinner = findViewById(R.id.start_spinner);
+        destinationSpinner = findViewById(R.id.destination_spinner);
+        seatsSpinner = findViewById(R.id.seats_spinner);
 
+        Button date_btn = (Button) findViewById(R.id.btn_date);
+        Button time_btn = (Button) findViewById(R.id.btn_time);
+        final EditText date_txt = (EditText) findViewById(R.id.in_date);
+        final EditText time_txt = (EditText) findViewById(R.id.in_time);
+
+        Button upload_btn = (Button) findViewById(R.id.ok_editor);
+
+        date_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int mYear, mMonth, mDay;
+                final Calendar calendar = Calendar.getInstance();
+                mYear = calendar.get(Calendar.YEAR);
+                mMonth = calendar.get(Calendar.MONTH);
+                mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(mContext, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        date_txt.setText(day + "-" + month + "-" + year);
+                    }
+                }, mYear, mMonth, mDay);
+
+                datePickerDialog.show();
+            }
+        });
+
+        time_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int mHour = calendar.get(Calendar.HOUR);
+                int mMinute = calendar.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(mContext, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                        time_txt.setText(hour + ":" + minute);
+                    }
+                }, mHour, mMinute, false);
+                timePickerDialog.show();
+            }
+        });
+
+        upload_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
@@ -31,6 +103,32 @@ public class CreatePostActivity extends AppCompatActivity implements AdapterView
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+        EditText memo_text = (EditText) findViewById(R.id.details_editor);
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        int radioSelected = radioGroup.getCheckedRadioButtonId();
 
+        Date postTime = new Date();
+        Date departTime = new Date();
+        String start = originSpinner.getSelectedItem().toString();
+        String dest = destinationSpinner.getSelectedItem().toString();
+        String memo = memo_text.getText().toString();
+        boolean driver_needed = (radioSelected == 0) ? false : true;
+        String driver = "";
+        String uploader = "";
+        ArrayList<String> passengers = new ArrayList<String>();
+        int totalSeats = Integer.parseInt(seatsSpinner.getSelectedItem().toString());
+
+        backendClient.createPost(new PostInfo(postTime, departTime, start, dest, memo, driver_needed, driver, uploader, passengers, totalSeats),
+                new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("hello", "hihi");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), (String) error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
