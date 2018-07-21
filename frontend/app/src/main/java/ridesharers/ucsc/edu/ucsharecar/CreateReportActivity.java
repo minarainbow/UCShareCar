0f4fc74d88877c4fb1b2a97ce38f3f884dce1eeb
minarainbow <minarainbow@kaist.ac.kr>
2018-07-21 16:29:31 -0700

    added report check alert

--- frontend/app/src/main/java/ridesharers/ucsc/edu/ucsharecar/CreateReportActivity.java
+++ frontend/app/src/main/java/ridesharers/ucsc/edu/ucsharecar/CreateReportActivity.java
@@ -1,5 +1,6 @@
 package ridesharers.ucsc.edu.ucsharecar;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.nfc.Tag;
@@ -19,11 +20,15 @@ import com.android.volley.VolleyError;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class CreateReportActivity extends AppCompatActivity {
 
     private final String TAG = "Report_Activity";
     private BackendClient backend;
+    private AlertDialog.Builder builder;
+    private AlertDialog popup;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,29 +46,42 @@ public class CreateReportActivity extends AppCompatActivity {
         upload_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    Log.d(TAG, "clicked on upload button from post");
-                    String title = title_text.getText().toString();
-                    String details = details_text.getText().toString();
-                    String contact = contact_text.getText().toString();
-                    final ReportInfo myReport = new ReportInfo(contact, title, details);
+                builder = new AlertDialog.Builder(CreateReportActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.report_upload_check,null);
+                Button yesButton = mView.findViewById(R.id.yes);
+                builder.setView(mView);
+                popup = builder.create();
 
-                    Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                        "mailto","abc@gmail.com", null));
-                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
-                    emailIntent.putExtra(Intent.EXTRA_TEXT, "Body");
-                    startActivity(Intent.createChooser(emailIntent, "Send email..."));
 
-                    backend.createReport(new ReportInfo(contact, title, details), new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            //Log.e(myReport.toString());
-                        }
-                    }, new ErrorListener() {
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
-                        }
-                    });
-            }
+                yesButton.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        Log.d(TAG, "clicked on upload button from post");
+                        String title = title_text.getText().toString();
+                        String details = details_text.getText().toString();
+                        String contact = contact_text.getText().toString();
+                        final ReportInfo myReport = new ReportInfo(contact, title, details);
+
+                        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                                "mailto","abc@gmail.com", null));
+                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+                        emailIntent.putExtra(Intent.EXTRA_TEXT, "Body");
+                        startActivity(Intent.createChooser(emailIntent, "Send email..."));
+
+                        backend.createReport(new ReportInfo(contact, title, details), new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                //Log.e(myReport.toString());
+                            }
+                        }, new ErrorListener() {
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                    }
+                });
+                            }
         });
 
 
